875a3bcf14065709fe6775f0017f0e5c77218763
Tanishq-vhd <janshu123@gmail.com>
2026-02-28 15:42:45 +0530

    Remove private key existence check and adjust private key formatting in Firebase configuration

--- backend/Firebase.js
+++ backend/Firebase.js
@@ -1,13 +1,9 @@
 import admin from "firebase-admin";
 
-if (!process.env.FIREBASE_PRIVATE_KEY) {
-  throw new Error("FIREBASE_PRIVATE_KEY is missing");
-}
-
 const serviceAccount = {
   projectId: process.env.FIREBASE_PROJECT_ID,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  privateKey: process.env.FIREBASE_PRIVATE_KEY,
 };
 
 admin.initializeApp({
